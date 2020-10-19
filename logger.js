de8642cf4cf8543849ab618a1b7bcb057fa148f0
choilm <choilmto@gmail.com>
2020-10-19 17:12:03 -0400

    Use refactored API 0.7.0

--- logger.js
+++ logger.js
@@ -3,5 +3,6 @@ const logger = require("pino")();
 logger.info({
     duration: process.env.JOB_DURATION,
     version: process.env.VERSION,
-    pull_requests: process.env.PULL_REQUESTS
+    pull_requests: process.env.PULL_REQUESTS,
+    job_name: process.env.JOB_NAME
 });
